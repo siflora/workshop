4ade8d0297d1c8d035197c86c2a6daf9783156ff
flora <si-han@hotmail.com>
2019-10-23 10:33:26 +0000

    23.10

--- single-chat.js
+++ single-chat.js
@@ -3,16 +3,15 @@ var feed = hypercore('./single-chat-feed', {
   valueEncoding: 'json'
 })
 
-feed.append({
-  type: 'chat-message',
-  nickname: 'cat-lover',
-  text: 'hello world',
-  timestamp: '2018-11-05T14:26:000Z' // new Date().toISOString()
-}, function (err, seq) {
-  if (err) throw err
-  console.log('Data was appended as entry #' + seq)
+process.stdin.on('data', function (data) {
+  feed.append({
+    type: 'chat-message',
+    nickname: 'cat-lover',
+    text: data.toString().trim(),
+    timestamp: new Date().toISOString()
+  })
 })
-feed.get(0, (err,data)=>{
-  if(err){return err};
-  return console.log(data)
-})
+feed.createReadStream({ live: true })
+  .on('data', function (data) {
+    console.log('<'+data.timestamp+'> '+data.nickname+': '+ data.text)
+  })
